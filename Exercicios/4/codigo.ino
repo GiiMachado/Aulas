caf01621ca7f91b009dfc40a9150081d4114e799
GiiMachado <80901675+GiiMachado@users.noreply.github.com>
2022-03-21 16:43:21 -0300

    Update codigo.ino

--- Exercicios/4/codigo.ino
+++ Exercicios/4/codigo.ino
@@ -1,1 +1,23 @@
+int button = 5;
+int led = 9;
+int lastButtonState = 0;
+int buttonState = 0;
+
+void setup() {
+  pinMode(led, OUTPUT);
+  pinMode(button, INPUT);
+}
+void loop() {
+  buttonState = digitalRead(button);
+    if (buttonState != lastButtonState) {
+        if (buttonState == HIGH) {
+       
+          digitalWrite(led, HIGH);
+        } else {
+          digitalWrite(led, LOW);
+        }
+        delay(5);
+        }
+    }
+    
 
