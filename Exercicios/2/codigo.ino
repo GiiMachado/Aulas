aa71c4028931d04ea9d4b7254fbb47ef98638c69
GiiMachado <80901675+GiiMachado@users.noreply.github.com>
2022-03-21 16:26:36 -0300

    Update codigo.ino

--- Exercicios/2/codigo.ino
+++ Exercicios/2/codigo.ino
@@ -1,1 +1,15 @@
+void setup()
+{
+  pinMode(4, OUTPUT);
+  pinMode(8, OUTPUT); 
+}
 
+void loop()
+{ 
+  digitalWrite(8, HIGH);
+  delay(10000); 
+  digitalWrite(8, LOW);
+  digitalWrite(4, HIGH);
+  delay(10000); 
+   digitalWrite(4, LOW);   
+}
